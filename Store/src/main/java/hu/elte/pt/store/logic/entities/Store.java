2bebc9ba7b51e81b4aae16751611f4cfc51e2553
Dudas Orsolya <>
2015-04-25 21:53:56 +0200

    Store entity added.

---
+++ Store/src/main/java/hu/elte/pt/store/logic/entities/Store.java
@@ -0,0 +1,76 @@
+package hu.elte.pt.store.logic.entities;
+
+import java.util.Objects;
+
+/**
+ * A Raktár tábla Java megfelelője.
+ * @author Dudás Orsolya
+ */
+
+public class Store implements Entity {
+    private Integer storeId;
+    private String name;
+    private String place;
+ 
+    public static final String[] fieldNames = new String[]{"Raktár"};
+
+    public Integer getStoreId() {
+        return storeId;
+    }
+
+    public void setStoreId(Integer storeId) {
+        this.storeId = storeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 79 * hash + Objects.hashCode(this.storeId);
+        hash = 79 * hash + Objects.hashCode(this.name);
+        hash = 79 * hash + Objects.hashCode(this.place);
+        return hash;
+    }
+
+@Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Store other = (Store) obj;
+        if (!Objects.equals(this.storeId, other.storeId)) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.place, other.place)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Store{" + "storeId=" + storeId + "," + "name=" + name + "," + "place=" + place + '}';
+    }
+    
+}
