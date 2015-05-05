24cc393303d34d3f9883b85a694c3950ed866fa7
holloeszter92@gmail.com <>
2015-05-05 21:55:32 +0200

    + test cases

--- Store/src/test/java/hu/elte/pt/store/test/logic/controller/CategoryControllerTest.java
+++ Store/src/test/java/hu/elte/pt/store/test/logic/controller/CategoryControllerTest.java
@@ -10,68 +10,63 @@ import org.junit.Test;
 
 /**
  *
- * @author Holló Eszter
- * Category Controllerhez tartozó tesztek
+ * @author Holló Eszter Category Controllerhez tartozó tesztek
  */
 public class CategoryControllerTest {
-    
+
     CategoryTableModel categoryTableModel = new CategoryTableModel();
-    
+
     public CategoryController categoryController = new CategoryController();
-       
-  
+
     @Test
     public void addAndRemoveTest() throws SQLException {
-      int initialNumberOfCategories = categoryController.getEntityCount();
-      
-      categoryController.addNewEntity();
-      Assert.assertEquals(initialNumberOfCategories + 1, categoryController.getEntityCount()); 
-       Assert.assertEquals(categoryTableModel.getRowCount(), categoryController.getEntityCount());
-    
-      int lastIndex = initialNumberOfCategories;
-      categoryController.deleteEntity(lastIndex);
-      Assert.assertEquals(initialNumberOfCategories, categoryController.getEntityCount()); 
-      Assert.assertEquals(categoryTableModel.getRowCount(), categoryController.getEntityCount());
-         
+        int initialNumberOfCategories = categoryController.getEntityCount();
+
+        categoryController.addNewEntity();
+        Assert.assertEquals(initialNumberOfCategories + 1, categoryController.getEntityCount());
+        Assert.assertEquals(categoryTableModel.getRowCount(), categoryController.getEntityCount());
+
+        int lastIndex = initialNumberOfCategories;
+        categoryController.deleteEntity(lastIndex);
+        Assert.assertEquals(initialNumberOfCategories, categoryController.getEntityCount());
+        Assert.assertEquals(categoryTableModel.getRowCount(), categoryController.getEntityCount());
+
     }
-    
+
     @Test(expected = Exception.class)
     public void removeWithInvalidIndexTest() throws SQLException {
-      categoryController.deleteEntity(categoryController.getEntityCount() + 1); 
+        categoryController.deleteEntity(categoryController.getEntityCount() + 1);
     }
-    
+
     @Test(expected = Exception.class)
     public void removeWithNegativeIndexTest() throws SQLException {
-      categoryController.deleteEntity(-1); 
+        categoryController.deleteEntity(-1);
     }
-    
-    
+
     @Test
     public void entityPropertiesTest() throws SQLException {
         List<Category> categories = categoryController.getEntities();
-        
-        for(Category category : categories){
+
+        for (Category category : categories) {
             Assert.assertNotNull(category.getCategoryId());
-            Assert.assertNotNull(category.getName());          
-        }     
-        
+            Assert.assertNotNull(category.getName());
+        }
+
     }
-    
+
     @Test
     public void getEntityTest() throws SQLException {
         int numberOfEntities = categoryController.getEntityCount();
-        for(int i = 0; i < numberOfEntities; i++){
+        for (int i = 0; i < numberOfEntities; i++) {
             Assert.assertNotNull(categoryController.getEntityByRowIndex(i));
         }
     }
-    
+
     @Test(expected = Exception.class)
     public void getEntityWithInvalidIndexTest() throws SQLException {
-        int numberOfEntities = categoryController.getEntityCount();        
-        Assert.assertNotNull(categoryController.getEntityByRowIndex(numberOfEntities));      
-  
-    }   
-    
-   
-   
-}   
+        int numberOfEntities = categoryController.getEntityCount();
+        Assert.assertNotNull(categoryController.getEntityByRowIndex(numberOfEntities));
+
+    }
+
+}
