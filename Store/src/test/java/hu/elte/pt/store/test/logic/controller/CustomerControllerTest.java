1784d28c70c495666f3708f0b53ea56aa31d2f2c
Eszti <Eszti@Eszti-PC>
2015-05-05 20:35:46 +0200

    Customer Controller tesztek

---
+++ Store/src/test/java/hu/elte/pt/store/test/logic/controller/CustomerControllerTest.java
@@ -0,0 +1,55 @@
+package hu.elte.pt.store.test.logic.controller;
+
+
+import hu.elte.pt.store.logic.controllers.CustomerController;
+import hu.elte.pt.store.logic.entities.Customer;
+import java.sql.SQLException;
+import java.util.List;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * @author Holló Eszter
+ * Customer Controllerhez tartozó tesztek
+ */
+public class CustomerControllerTest {
+    
+    public CustomerController customerController = new CustomerController();
+       
+  
+    @Test
+    public void addAndRemoveTest() throws SQLException {
+      int initialNumberOfCustomers = customerController.getEntityCount();
+      
+      customerController.addNewEntity();
+      Assert.assertEquals(initialNumberOfCustomers + 1, customerController.getEntityCount());      
+    
+      int lastIndex = initialNumberOfCustomers;
+      customerController.deleteEntity(lastIndex);
+      Assert.assertEquals(initialNumberOfCustomers, customerController.getEntityCount());          
+         
+    }
+    
+    @Test(expected = Exception.class)
+    public void removeWithInvalidIndexTest() throws SQLException {
+      customerController.deleteEntity(customerController.getEntityCount() + 1); 
+    }
+    
+    @Test
+    public void entityPropertiesTest() throws SQLException {
+        List<Customer> customers = customerController.getEntities();
+        
+        for(Customer customer : customers){
+            Assert.assertNotNull(customer.getCustomerID());
+            Assert.assertNotNull(customer.getAddress());
+            Assert.assertNotNull(customer.getEmail());
+            Assert.assertNotNull(customer.getName());
+            Assert.assertNotNull(customer.getTelephone());
+        }     
+        
+    }
+    
+    
+   
+}   
