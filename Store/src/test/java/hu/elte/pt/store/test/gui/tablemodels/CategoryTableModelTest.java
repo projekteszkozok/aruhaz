7dfb60131c7f6565009d638b74ada548eb5f0f00
Eszti <Eszti@Eszti-PC>
2015-05-05 23:14:52 +0200

    test fix

--- Store/src/test/java/hu/elte/pt/store/test/gui/tablemodels/CategoryTableModelTest.java
+++ Store/src/test/java/hu/elte/pt/store/test/gui/tablemodels/CategoryTableModelTest.java
@@ -18,24 +18,40 @@ public class CategoryTableModelTest {
     CategoryTableModel categoryTableModel = new CategoryTableModel();
 
     @Test
-    public void columnTest(){
+    public void columnTest() {
         int columnCount = categoryTableModel.getColumnCount();
-        
-        for(int i = 0; i < columnCount-1; i++){
+        for (int i = 0; i < columnCount; i++) {
             Assert.assertNotNull(categoryTableModel.getColumnName(i));
         }
     }
-    
+
+    @Test(expected = Exception.class)
+    public void invalidColumnIndexTest() {
+        int columnCount = categoryTableModel.getColumnCount();
+        Assert.assertNull(categoryTableModel.getColumnName(columnCount + 1));
+    }
+
     @Test
-    public void rowAndColumnTest(){
+    public void rowAndColumnTest() {
         int rowCount = categoryTableModel.getRowCount();
         int columnCount = categoryTableModel.getColumnCount();
-        
-        for(int i = 0; i < rowCount-1; i++){
-            for(int j = 0; j < columnCount-1; j++){
-                Assert.assertNotNull(categoryTableModel.getValueAt(i, j));
+
+        if(rowCount > 1 && columnCount > 1){
+            for (int i = 1; i < rowCount-1; i++) {
+                for (int j = 1; j < columnCount-1; j++) {
+                    Assert.assertNotNull(categoryTableModel.getValueAt(i, j));
+                }
             }
         }
+    }
+
+    @Test(expected = Exception.class)
+    public void invalidrowAndColumnIndexTest() {
+        int rowCount = categoryTableModel.getRowCount();
+        int columnCount = categoryTableModel.getColumnCount();
+
+        Assert.assertNull(categoryTableModel.getValueAt(rowCount + 1, columnCount + 1));
+
     }
 
 }
