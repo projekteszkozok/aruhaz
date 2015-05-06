7793324e7c28c29b31b2284dab3f4c343696669b
Honti Dóra <honti.dora@virgo.hu>
2015-05-06 13:12:01 +0200

    Manufacturer Controller tesztek

--- Store/src/test/java/hu/elte/pt/store/test/logic/controller/ManufacturerControllerTest.java
+++ Store/src/test/java/hu/elte/pt/store/test/logic/controller/ManufacturerControllerTest.java
@@ -1,5 +1,6 @@
 package hu.elte.pt.store.test.logic.controller;
 
+import hu.elte.pt.store.gui.tablemodels.ManufacturerTableModel;
 import hu.elte.pt.store.logic.controllers.ManufacturerController;
 import hu.elte.pt.store.logic.entities.Manufacturer;
 import java.sql.SQLException;
@@ -14,37 +15,61 @@ import org.junit.Test;
  */
 public class ManufacturerControllerTest {
 
+    ManufacturerTableModel manufacturerTableModel = new ManufacturerTableModel();    
     public ManufacturerController manufacturerController = new ManufacturerController();
 
-    @Test
+     @Test
     public void addAndRemoveTest() throws SQLException {
-        int initialNumberOfManufacturers = manufacturerController.getEntityCount();
-
-        manufacturerController.addNewEntity();
-        Assert.assertEquals(initialNumberOfManufacturers + 1, manufacturerController.getEntityCount());
-
-        int lastIndex = initialNumberOfManufacturers;
-        manufacturerController.deleteEntity(lastIndex);
-        Assert.assertEquals(initialNumberOfManufacturers, manufacturerController.getEntityCount());
-
+      int initialNumberOfManufacturers = manufacturerController.getEntityCount();
+      
+      manufacturerController.addNewEntity();
+      Assert.assertEquals(initialNumberOfManufacturers + 1, manufacturerController.getEntityCount());      
+      Assert.assertEquals(manufacturerController.getEntityCount(), manufacturerTableModel.getRowCount());
+      
+      int lastIndex = initialNumberOfManufacturers;
+      manufacturerController.deleteEntity(lastIndex);
+      Assert.assertEquals(initialNumberOfManufacturers, manufacturerController.getEntityCount());     
+      Assert.assertEquals(manufacturerController.getEntityCount(), manufacturerTableModel.getRowCount());
+              
     }
-
+    
     @Test(expected = Exception.class)
     public void removeWithInvalidIndexTest() throws SQLException {
-        manufacturerController.deleteEntity(manufacturerController.getEntityCount() + 1);
+      manufacturerController.deleteEntity(manufacturerController.getEntityCount() + 1); 
     }
-
+    
+    @Test(expected = Exception.class)
+    public void removeWithNegativeIndexTest() throws SQLException {
+      manufacturerController.deleteEntity(-1); 
+    }
+    
     @Test
     public void entityPropertiesTest() throws SQLException {
         List<Manufacturer> manufacturers = manufacturerController.getEntities();
-
-        for (Manufacturer manufacturer : manufacturers) {
+        
+        for(Manufacturer manufacturer : manufacturers){
             Assert.assertNotNull(manufacturer.getManufacturerId());
             Assert.assertNotNull(manufacturer.getName());
             Assert.assertNotNull(manufacturer.getContactName());
             Assert.assertNotNull(manufacturer.getCity());
             Assert.assertNotNull(manufacturer.getPhone());
             Assert.assertNotNull(manufacturer.getStore());
+        }     
+        
+    }
+    
+    @Test
+    public void getEntityTest() throws SQLException {
+        int numberOfEntities = manufacturerController.getEntityCount();
+        for(int i = 0; i < numberOfEntities; i++){
+            Assert.assertNotNull(manufacturerController.getEntityByRowIndex(i));
         }
+    }
+    
+    @Test(expected = Exception.class)
+    public void getEntityWithInvalidIndexTest() throws SQLException {
+        int numberOfEntities = manufacturerController.getEntityCount();
+        Assert.assertNotNull(manufacturerController.getEntityByRowIndex(numberOfEntities + 1));
+        
     }
 }
