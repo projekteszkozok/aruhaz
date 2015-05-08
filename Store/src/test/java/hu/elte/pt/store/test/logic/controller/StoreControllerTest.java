d9570251feaff927de9697447019734dafed709a
orsolya.dudas <orsi1293@gmail.com>
2015-05-09 00:48:46 +0200

    storecontroller test fix

--- Store/src/test/java/hu/elte/pt/store/test/logic/controller/StoreControllerTest.java
+++ Store/src/test/java/hu/elte/pt/store/test/logic/controller/StoreControllerTest.java
@@ -1,5 +1,6 @@
 package hu.elte.pt.store.test.logic.controller;
 
+
 import hu.elte.pt.store.gui.tablemodels.StoreTableModel;
 import hu.elte.pt.store.logic.controllers.StoreController;
 import hu.elte.pt.store.logic.entities.Store;
@@ -10,62 +11,66 @@ import org.junit.Test;
 
 /**
  *
- * @author Dudás Orsolya Store Controllerhez tesztek
+ * @author Dudás Orsolya
+ * Store Controllerhez tartozó tesztek
  */
 public class StoreControllerTest {
-
-    StoreTableModel storeTableModel = new StoreTableModel();
+    
+    StoreTableModel storeTableModel = new StoreTableModel();    
     public StoreController storeController = new StoreController();
-
+       
+  
     @Test
     public void addAndRemoveTest() throws SQLException {
-        int initialNumberOfStores = storeController.getEntityCount();
-
-        storeController.addNewEntity();
-        Assert.assertEquals(initialNumberOfStores + 1, storeController.getEntityCount());
-        Assert.assertEquals(storeTableModel.getRowCount(), storeController.getEntityCount());
-
-        int lastIndex = initialNumberOfStores;
-        storeController.deleteEntity(lastIndex);
-        Assert.assertEquals(initialNumberOfStores, storeController.getEntityCount());
-        Assert.assertEquals(storeTableModel.getRowCount(), storeController.getEntityCount());
-
+      int initialNumberOfStores = storeController.getEntityCount();
+      
+      storeController.addNewEntity();
+      Assert.assertEquals(initialNumberOfStores + 1, storeController.getEntityCount());      
+      Assert.assertEquals(storeController.getEntityCount(), storeTableModel.getRowCount());
+      
+      int lastIndex = initialNumberOfStores;
+      storeController.deleteEntity(lastIndex);
+      Assert.assertEquals(initialNumberOfStores, storeController.getEntityCount());     
+      Assert.assertEquals(storeController.getEntityCount(), storeTableModel.getRowCount());
+              
     }
-
+    
     @Test(expected = Exception.class)
     public void removeWithInvalidIndexTest() throws SQLException {
-        storeController.deleteEntity(storeController.getEntityCount() + 1);
+      storeController.deleteEntity(storeController.getEntityCount() + 1); 
     }
-
+    
     @Test(expected = Exception.class)
     public void removeWithNegativeIndexTest() throws SQLException {
-        storeController.deleteEntity(-1);
+      storeController.deleteEntity(-1); 
     }
-
+    
     @Test
     public void entityPropertiesTest() throws SQLException {
         List<Store> stores = storeController.getEntities();
-
-        for (Store store : stores) {
+        
+        for(Store store : stores){
             Assert.assertNotNull(store.getStoreId());
             Assert.assertNotNull(store.getName());
             Assert.assertNotNull(store.getPlace());
-        }
-
+        }     
+        
     }
-
+    
     @Test
     public void getEntityTest() throws SQLException {
         int numberOfEntities = storeController.getEntityCount();
-        for (int i = 0; i < numberOfEntities; i++) {
+        for(int i = 0; i < numberOfEntities; i++){
             Assert.assertNotNull(storeController.getEntityByRowIndex(i));
         }
     }
-
+    
     @Test(expected = Exception.class)
     public void getEntityWithInvalidIndexTest() throws SQLException {
         int numberOfEntities = storeController.getEntityCount();
         Assert.assertNotNull(storeController.getEntityByRowIndex(numberOfEntities + 1));
-
+        
     }
-}
+    
+   
+}   
