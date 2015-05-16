2b1d2d4dc85e1ef724eb0b55facdaa8e54be22f3
orsolya.dudas <orsi1293@gmail.com>
2015-05-16 03:45:38 +0200

    StoreTableModelTest add

---
+++ Store/src/test/java/hu/elte/pt/store/test/gui/tablemodels/StoreTableModelTest.java
@@ -0,0 +1,41 @@
+package hu.elte.pt.store.test.gui.tablemodels;
+
+
+import hu.elte.pt.store.gui.tablemodels.StoreTableModel;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * @author Dudás Orsi
+ */
+public class StoreTableModelTest {
+    
+    StoreTableModel storeTableModel = new StoreTableModel();
+
+    
+    @Test
+    public void columnTest(){
+        int columnCount = storeTableModel.getColumnCount();
+        
+        for(int i = 0; i < columnCount-1; i++){
+            Assert.assertNotNull(storeTableModel.getColumnName(i));
+        }
+    }
+    
+    @Test
+    public void rowAndColumnTest(){
+        int rowCount = storeTableModel.getRowCount();
+        int columnCount = storeTableModel.getColumnCount();
+        
+        if(rowCount > 1 && columnCount > 1){
+            
+            for(int j = 1; j < columnCount-1; j++){
+                Assert.assertNotNull(storeTableModel.getColumnName(j));
+            }
+        
+        }
+        
+    }
+
+}
